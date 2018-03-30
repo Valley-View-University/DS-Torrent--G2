31519afb046e9cfa7ea2f3f249c3d3b506222821
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-03-30 20:23:46 +0000

    main commit

---
+++ helpers/torrentParser.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+/**
+ * Converts the params uri passed to torrent
+ */
+function parseTorrent(uri) {
+
+}
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Torrent App'
+  });
+});
+
+router.get('/search', function (req, res) {
+  res.send('Searching Database');
 });
 
 module.exports = router;
